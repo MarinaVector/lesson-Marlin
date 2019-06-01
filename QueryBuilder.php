d3684a686d78b8958a8ecfbf9597eb296a997cdd
Marinacvbn1811 <beluana1@mail.ru>
2019-06-01 22:42:21 +0500

    second

--- QueryBuilder.php
+++ QueryBuilder.php
@@ -14,12 +14,12 @@ $this->pdo = $pdo;
         $statement = $this->pdo->prepare($sql);
         $statement->execute();
         return $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-        //return $results;
+       
     }
 
 
 public function getOne($table, $id) {
-	//dd([$table, $id]);
+	
 	$sql = "SELECT * FROM posts WHERE id=:id";
 	$statement = $this->pdo->prepare($sql);
 	$statement->bindValue(':id' , $id);
@@ -34,14 +34,10 @@ public function create($table, $data)
    $tags = ":".implode (',:', array_keys($data));
 
 	$sql = "INSERT INTO {$table} ({$keys})  VALUES ({$tags})";
-	//$keys = array_keys($data);
 	
-	//dd($sql);
-	 //$statement = $this->pdo->query($sql);
 	$statement = $this->pdo->prepare($sql);
-	 //$statement->bindValue(':title', 'qweqwe'); //привязать значение к метке тайтл
-	 $statement->execute($data);
- //var_dump($statement);
+		 $statement->execute($data);
+ 
 
  
 }
@@ -68,11 +64,9 @@ $data['id'] = $id;
 
 }
 public function delete($posts, $id) {
-	//$sql = "DELETE * FROM {$table} WHERE id=:id";
-    $sql =  "DELETE FROM `posts` WHERE `posts`.`id` = :id";
+	    $sql =  "DELETE FROM `posts` WHERE `posts`.`id` = :id";
 	$statement = $this->pdo->prepare($sql);
-	//$statement->bindValue(':id' , $id);
-	$statement->execute(['id' => $id]);
+		$statement->execute(['id' => $id]);
 	
 	
 		
--- route.php
+++ route.php
@@ -1,8 +1,45 @@
 <?php
-return [
-    'home' => 'index',
-    'page/1' => page1
 
-
-];
- ?>
+class Router {
+	
+	private $route;
+	
+	public function __construct(){
+		$this->route = $_SERVER['REQUEST_URI'];
+	}
+	
+	public function getId(){
+	
+	$result = ltrim($this->route, '/');
+		$string = explode('/', $result);
+		return $id = $string['1'];
+	
+	}
+	
+	
+	public function createRoutes(){
+	
+	$id = $this->getId();
+	$routes = [
+      
+     "/home/page" => "home.php",
+     "/home/view/posts" => "index.view.php",
+     "/create/new/post" => "create.php",
+     "/edit/old/post"=> "edit.php",
+     "/show/existing/post" => "show.php",
+      "/update/new/post/$id" => 'update.php',
+      "/show/existing/post/$id" => 'show.php',
+      "/edit/old/post/$id" => 'edit.php',
+      "/delete/old/post/$id" => 'delete.php',
+      "/404/error" => '404.php'
+      	];
+	
+      if(array_key_exists($this->route, $routes)){
+		include '../' . $routes[$this->route];
+      exit;
+	}else{
+		include '../404.php';
+	}	
+	
+	}
+}
